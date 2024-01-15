ee7eb97196aaa8964811b54bfb10df0b1c4c821b
1707akash <1707akash@gmail.com>
2024-01-15 17:18:54 +0530

    styling

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 // console.log("hii");
 
 let heading= document.querySelector("h1");
-let codeDiv = document.querySelector(".code");
-let keyText = document.createElement("p");
-codeDiv.appendChild(keyText);
+let keyText = document.querySelector("p");
 
 document.body.addEventListener("keydown",(e)=>{
     let pressedKey = e.key;
     if(e.shiftKey && e.keyCode!=16){
         pressedkey = `Shift+${e.key}`
     }
-    if(e.ctrlKey&& e.keyCode!=17){
+    if(e.ctrlKey&& e.keyCode!=17){  
         pressedkey = `Ctrl+${e.key}`
     };
-    heading.innerText= `You Pressed ${pressedKey}`;
-    heading.style.color= "Orangered";
+    heading.innerHTML= `You Pressed <span>${pressedKey}</span> `;
+    heading.style.color= "blue";
     
     keyText.innerText= e.keyCode;
     keyText.style.color= "green";
+    keyText.classList.add("code");
     
 })
